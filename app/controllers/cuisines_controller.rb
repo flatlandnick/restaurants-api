c2bf9a8fd7366a43ac45303b43efdbc8ba38ba76
flatlandnick <nicholas.lunn@flatironschool.com>
2021-02-04 12:20:16 -0600

    done

--- app/controllers/cuisines_controller.rb
+++ app/controllers/cuisines_controller.rb
@@ -7,6 +7,28 @@ class CuisinesController < ApplicationController
         render json: CuisineSerializer.new(cuisines, {include: [:restaurants]})
     end
 
+    def create
+        cuisine = Cuisine.new(cuisine_params)
+        if cuisine.save
+            render json: CuisineSerializer.new(cuisine)
+        else
+            render json: {errors: cuisine.errors}
+        end
+    end
+
     def show
+    end
+
+    def destroy
+        cuisine = Cuisine.find_by(id: params[:id].to_i)
+        cuisine.destroy
+        render json: {message: "The #{cuisine.name} cuisine has been deleted"}
+        # byebug
+    end
+
+    private
+    
+    def cuisine_params
+        params.require(:cuisine).permit(:name)
     end
 end
